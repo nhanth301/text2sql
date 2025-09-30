75161631600282c25a9a2fe50e0ac55f261dac46
nhanth301 <nhanthien.tnn@gmail.com>
2025-09-30 12:52:25 +0700

    update func anot

--- src/db/service.py
+++ src/db/service.py
@@ -1,16 +1,16 @@
 from src.db.client import engine
-from src.schema.output_schema import QueryResponse
+from src.schema.output_schema import QueryResponse, SQLGenerationSchema
 import pandas as pd
 from sqlalchemy.exc import SQLAlchemyError
 
-def query(sql_query: str, return_dict: bool = False) -> QueryResponse:
+def query(sql_query: SQLGenerationSchema, return_dict: bool = False) -> QueryResponse | dict :
     try:
         result = pd.read_sql(sql_query, engine)
         if return_dict:
             result = result.to_dict()
         return QueryResponse(result=result, error=None)
     except SQLAlchemyError as e:
-        return QueryResponse(result=None, error=str(e.__cause__ or e))
+        return QueryResponse(result=None, error=str(e.__cause__ or e)) if not return_dict else {'error': str(e.__cause__ or e)}
     except Exception as e:
-        return QueryResponse(result=None, error=str(e))
+        return QueryResponse(result=None, error=str(e)) if not return_dict else {'error': str(e)}
 
